63975d3349835049a2e79340388a180f960be6c9
Chris Weise <cweise@Chriss-iMac.attlocal.net>
2024-10-09 12:33:46 -0500

    Updated counter code... by removing a line that was causing an error that I didn't need and then adding the Api Gateway endpoint at the end of the fetch so it had the path of the stage name and the endpoint.

--- Frontend/website/js/counter.js
+++ Frontend/website/js/counter.js
@@ -2,17 +2,28 @@ const countDiv = document.querySelector(".counter");
 
 async function updateCount() {
     try {
-        let response = await fetch("https://5u5z1zn3k3.execute-api.us-east-1.amazonaws.com/chrisweisecloudresume/", {
+        let response = await fetch("https://e5fazlk3rg.execute-api.us-east-1.amazonaws.com/prod/count", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json' 
             }
         });
 
-        let data = await response.json();
-        countDiv.innerHTML = `This page has been visited ${data.new_count} times.`; 
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-    } 
+        let responseBody = await response.text(); 
+        let data = JSON.parse(responseBody); 
+
+        countDiv.innerHTML = `This page has been visited ${data.new_count} times!`; 
+
+    } catch (error) {
+        console.error('Error fetching the count:', error);
+        countDiv.innerHTML = "Error loading visit count.";
+    }
 }
 
 updateCount();
+
+
