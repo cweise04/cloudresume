e0ea554e969a5d87ade29b6c12f9695c9cece75b
Chris Weise <cweise@unknown7ae17ed01591.attlocal.net>
2024-12-17 10:33:41 -0600

    Added terraform files

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c7d65245f1fa10ad8a2a683364af27e0fd25f076d73f781047906b1745a8735d
+size 1406
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f476505fce53db228f84916f724fc83fe9e55e843aa92a5e7ba932c0cdb9c535
+size 1084
